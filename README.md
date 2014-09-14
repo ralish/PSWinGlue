b071d93c19867a7c4af39bdbda9dc8d8ee9a96be
Samuel D. Leslie <sdl@nexiom.net>
2014-09-14 18:18:46 +1000

    Add the first cmdlet: Get-TaskSchedulerEvent
    Simplifies pulling interesting Task Scheduler events out of the Event Log.
    Particularly useful when combined with event triggers to facilitate simple
    alerting of scheduled task failures.

---
+++ README.md
@@ -0,0 +1,12 @@
+PoSh-WinGlue
+============
+
+PowerShell Module with assorted random functions I find useful.
+
+In perpetual early development.
+
+
+Requirements
+============
+
+ * PowerShell 3.0 or newer
---
+++ WinGlue.psm1
@@ -0,0 +1,25 @@
+﻿Function Get-TaskSchedulerEvent {
+    Param(
+        [Parameter(Mandatory=$true)]
+            [Int32[]]$EventIds,
+        [Int32]$MaxEvents=1,
+        [String]$LogName='Microsoft-Windows-TaskScheduler/Operational',
+        [String[]]$IgnoredTasks=('\Microsoft\Windows\Shell\CreateObjectTask')
+    )
+
+    $ErrorActionPreference = 'Stop'
+
+    # Fetch the most recent matching scheduled task event
+    Write-Verbose 'Fetching matching task scheduler events...'
+    $Event = Get-WinEvent -FilterHashTable @{ LogName = $LogName; ID = $EventIds } -MaxEvents $MaxEvents
+
+    # Exit if the event matches any defined ignored tasks
+    Write-Verbose 'Filtering out any ignored task events...'
+    $EventXml = [xml]$Event.ToXml()
+    $EventTaskName = $EventXml.Event.EventData.Data | ? {$_.Name -eq 'TaskName'}
+    if ($EventTaskName.'#text' -in $IgnoredTasks) {
+        return
+    } else {
+        return $Event
+    }
+}
