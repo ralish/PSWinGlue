59b275ed41bdaad88f5fdc53126f847a8730ed29
Samuel D. Leslie <sdl@nexiom.net>
2015-01-24 22:31:41 +1100

    Fix stupid issue in Get-ChildItem & tweak module manifest

--- WinGlue.psm1
+++ WinGlue.psm1
@@ -1,4 +1,4 @@
 # Ensure all modules files are unblocked
 Get-ChildItem -Path $PSScriptRoot | Unblock-File
 # Source in all the module script files
-Get-ChildItem -Path $PSScriptRoot -Include '*.ps1' | ForEach-Object { . $_.FullName }
+Get-ChildItem -Path $PSScriptRoot\*.ps1 | ForEach-Object { . $_.FullName }
