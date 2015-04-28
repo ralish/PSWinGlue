167a74ade23a950d551fb0834019bbe7e689fe9d
Samuel D. Leslie <sdl@nexiom.net>
2015-04-28 17:54:47 +1000

    Some minor tweaks to Get-TaskSchedulerEvent

--- Get-TaskSchedulerEvent.ps1
+++ Get-TaskSchedulerEvent.ps1
@@ -2,32 +2,30 @@
     <#
         .Synopsis
         Fetches events matching given IDs from the Task Scheduler event log.
-        .Description
         .Parameter EventIds
-        An array of integers specifying the event IDs we want to match in the query. A particularly
-        useful set is "(111,202,203,323,329,331)", which corresponds to all current event IDs which
-        represent some form of failure to complete a scheduled task.
-        See: http://technet.microsoft.com/en-us/library/dd363729%28v=ws.10%29.aspx
+        An array of integers specifying the event IDs we want to match in the query. The default is
+        "(111,202,203,323,329,331)" which corresponds to all event IDs which represent a failure to
+        complete a scheduled task. This is particularly useful for identifying task failures.
         .Parameter MaxEvents
         Specifies the maximum number of events to return. Note that any filtering of returned events
         by the IgnoredTasks parameter is performed after the specified maximum number of events have
         been returned. As such, you cannot rely on receiving the maximum number of results as set by
         MaxEvents, even if there are enough events in the Event Log with filtering applied.
-        .Parameter LogName
-        The Event Log name to query; the default should rarely if ever need to be changed.
         .Parameter IgnoredTasks
         An array of strings specifying the task names we wish to exclude from the returned results.
         .Notes
+        For more information on Task Scheduler event IDs consult the TechNet documentation at:
+        http://technet.microsoft.com/en-us/library/dd363729%28v=ws.10%29.aspx
+
     #>
     Param(
-        [Parameter(Mandatory=$true)]
-            [Int32[]]$EventIds,
-        [Int32]$MaxEvents=1,
-        [String]$LogName='Microsoft-Windows-TaskScheduler/Operational',
+        [Int32[]]$EventIds=(111,202,203,323,329,331),
+        [Int32]$MaxEvents=10,
         [String[]]$IgnoredTasks=('\Microsoft\Windows\Shell\CreateObjectTask')
     )
 
     $ErrorActionPreference = 'Stop'
+    $LogName = 'Microsoft-Windows-TaskScheduler/Operational',
 
     # Fetch the most recent matching scheduled task event
     Write-Verbose 'Fetching matching task scheduler events...'
