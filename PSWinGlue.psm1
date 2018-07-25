64f9ebd33afb9b825dca4f7135b66607a4c27bd8
Samuel D. Leslie <sdl@nexiom.net>
2018-07-25 15:57:15 +1000

    Import scripts as functions on module load

--- PSWinGlue.psm1
+++ PSWinGlue.psm1
@@ -3,3 +3,9 @@ $Functions = Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Func
 foreach ($Function in $Functions) {
     . $Function.FullName
 }
+
+# Import all scripts
+$Scripts = Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Scripts') -File
+foreach ($Script in $Scripts) {
+    New-Item -Path ('Function:\{0}' -f $Script.BaseName) -Value (Get-Content -Path $Script.FullName -Raw)
+}
