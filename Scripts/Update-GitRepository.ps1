57bd9e29fb757b153277ce72849e21df3821368f
Samuel D. Leslie <sdl@nexiom.net>
2019-01-25 11:45:48 +1100

    Miscellaneous improvements to Update-GitRepository

--- Scripts/Update-GitRepository.ps1
+++ Scripts/Update-GitRepository.ps1
@@ -1,41 +1,21 @@
 [CmdletBinding()]
 Param()
 
-# Registry keys potentially containing Git installation details
-$GitInstallRegKeys = @(
-    # User: Native bitness
-    'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Git_is1',
-    # User: x86 on x64
-    'HKCU:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Git_is1',
-    # Machine: Native bitness
-    'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Git_is1',
-    # Machine: x86 on x64
-    'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Git_is1'
-)
-
-# Registry property which provides the installation directory
-$GitInstallRegProperty = 'InstallLocation'
-
 Function Initialize-GitEnvironment {
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory)]
         [String]$Path
     )
 
-    Write-Verbose -Message 'Initializing Git environment ...'
-
     # Amend the PATH variable to include the full set of Git utilities
     $Env:Path = '{0}\bin;{1}' -f $Path, $Env:Path
 
     # Create the HOME environment variable needed by SSH (via Git)
     #
-    # Note that if running as a Scheduled Task the user profile of the
-    # account we're running under may not yet be loaded on Windows 8 or
-    # Server 2012 and newer. As a result, the USERPROFILE environment
-    # variable will point to the Default user profile. We can work around
-    # this by using GetFolderPath() with the UserProfile enumeration.
+    # If running as a Scheduled Task the user profile of the account may not yet be loaded on
+    # Windows 8 or Server 2012 and newer. This results in the USERPROFILE environment variable
+    # pointing to the Default user profile. We can work around this by using GetFolderPath().
     #
     # See: https://support.microsoft.com/en-us/kb/2968540
     $Env:HOME = [Environment]::GetFolderPath([Environment+SpecialFolder]::UserProfile)
@@ -45,10 +25,21 @@ Function Test-GitInstalled {
     [CmdletBinding()]
     Param()
 
-    Write-Verbose -Message 'Testing Git is installed ...'
+    # Registry keys potentially containing Git installation details
+    $GitRegistryKeys = @(
+        # User
+        'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Git_is1',
+        # Machine: Native bitness
+        'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Git_is1',
+        # Machine: x86 on x64
+        'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Git_is1'
+    )
 
-    foreach ($RegKey in $GitInstallRegKeys) {
-        $GitInstallPath = (Get-ItemProperty -Path $RegKey -ErrorAction Ignore).$GitInstallRegProperty
+    # Registry property which contains the installation directory
+    $GitInstallProperty = 'InstallLocation'
+
+    foreach ($RegKey in $GitRegistryKeys) {
+        $GitInstallPath = (Get-ItemProperty -Path $RegKey -ErrorAction Ignore).$GitInstallProperty
 
         if ($GitInstallPath) {
             break
@@ -70,19 +61,16 @@ Function Test-GitRepository {
     [CmdletBinding()]
     Param()
 
-    Write-Verbose -Message 'Testing current directory is part of a Git repository ...'
-
     $null = & git rev-parse --git-dir 2>&1
     if ($LASTEXITCODE -ne 0) {
-        throw 'The current directory is not part of a Git repository.'
+        throw ('The current directory does not belong to a Git repository: {0}' -f $PWD.Path)
     }
 }
 
 Function Update-GitRepository {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
     [CmdletBinding()]
     Param()
-
-    Write-Verbose -Message 'Updating Git repository ...'
 
     & git add --all
     if ($LASTEXITCODE -ne 0) {
@@ -101,23 +89,23 @@ Function Update-GitRepository {
 
     & git pull
     if ($LASTEXITCODE -ne 0) {
-        throw 'Something went wrong pulling from the Git repository.'
+        throw 'Something went wrong pulling from the remote Git repository.'
     }
 
     & git push
     if ($LASTEXITCODE -ne 0) {
-        throw 'Something went wrong pushing to the Git repository.'
+        throw 'Something went wrong pushing to the remote Git repository.'
     }
 }
 
-# Check Git is installed
+# Test Git software is installed
 $GitInstallPath = Test-GitInstalled
 
-# Check we're operating within a Git repository
+# Check we're in a Git repository
 Test-GitRepository
 
 # Initialize the Git environment
 Initialize-GitEnvironment -Path $GitInstallPath
 
-# Commit all changes and update the Git repository
+# Commit all and push all changes
 Update-GitRepository
