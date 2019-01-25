f3a4f9da7fe2f765275dd17d1a11a9776a5551d2
Samuel D. Leslie <sdl@nexiom.net>
2019-01-25 12:02:35 +1100

    Remove defunct sourcing of functions

--- PSWinGlue.psm1
+++ PSWinGlue.psm1
@@ -1,10 +1,4 @@
-# Import all functions
-$Functions = Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Functions') -File
-foreach ($Function in $Functions) {
-    . $Function.FullName
-}
-
-# Import all scripts
+# Import all scripts as functions
 $Scripts = Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Scripts') -File
 foreach ($Script in $Scripts) {
     New-Item -Path ('Function:\{0}' -f $Script.BaseName) -Value (Get-Content -Path $Script.FullName -Raw)
