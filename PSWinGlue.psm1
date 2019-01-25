b21e4ad333a38e5e4129ce8bc4a736d4000274ea
Samuel D. Leslie <sdl@nexiom.net>
2019-01-25 12:22:05 +1100

    Enable strict mode for the module

--- PSWinGlue.psm1
+++ PSWinGlue.psm1
@@ -1,3 +1,6 @@
+# See the help for Set-StrictMode for the full details on what this enables.
+Set-StrictMode -Version 2.0
+
 # Import all scripts as functions
 $Scripts = Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Scripts') -File
 foreach ($Script in $Scripts) {
