d7946424c8ecaa8d2f12ec2da7d997ea181c146b
Samuel D. Leslie <sdl@nexiom.net>
2022-07-25 09:36:28 +1000

    Register-MicrosoftUpdate: Major clean-up & add documentation

--- Scripts/Register-MicrosoftUpdate.ps1
+++ Scripts/Register-MicrosoftUpdate.ps1
@@ -1,28 +1,74 @@
+<#
+    .SYNOPSIS
+    Register the Microsoft Update service with the Windows Update Agent
+
+    .DESCRIPTION
+    The Windows Update Agent (WUA) has a concept of "services" which, once registered with WUA, add additional catalogues of updates for delivery to the system.
+
+    By default, the Windows Update service is registered to provide updates to the operating system. To receive updates for other installed Microsoft products the Microsoft Update service must be registered.
+
+    This script automates the registration of the Microsoft Update service with WUA to ensure the operating system and all installed Microsoft products receive available updates.
+
+    .EXAMPLE
+    Register-MicrosoftUpdate
+
+    Ensures the Microsoft Update service is registered with the Windows Update Agent.
+
+    .NOTES
+    Administrator privileges are required to modify the Windows Update Agent configuration.
+
+    .LINK
+    https://github.com/ralish/PSWinGlue
+#>
+
 #Requires -Version 3.0
 #Requires -RunAsAdministrator
 
 [CmdletBinding()]
+[OutputType()]
 Param()
 
-# https://docs.microsoft.com/en-us/windows/desktop/wua_sdk/opt-in-to-microsoft-update
+# Opt-In to Microsoft Update
+# https://docs.microsoft.com/en-us/windows/win32/wua_sdk/opt-in-to-microsoft-update
+$MuServiceId = '7971f918-a847-4430-9279-4a52d1efe18d'
 
-# Microsoft Update identifier
-$ServiceID = '7971f918-a847-4430-9279-4a52d1efe18d'
 # AddServiceFlag enumeration
-# https://docs.microsoft.com/en-au/windows/desktop/api/wuapi/ne-wuapi-tagaddserviceflag
-$ServiceFlags = 7
+# https://docs.microsoft.com/en-us/windows/win32/api/wuapi/ne-wuapi-addserviceflag
+$asfAllowPendingRegistration = 1
+$asfAllowOnlineRegistration = 2
+$asfRegisterServiceWithAU = 4
+$MuServiceFlags = $asfAllowPendingRegistration + $asfAllowOnlineRegistration + $asfRegisterServiceWithAU
+
+$WuaServiceManager = $null
+$WuaServices = $null
+$MuService = $null
 
 try {
-    $ServiceManager = New-Object -ComObject Microsoft.Update.ServiceManager
-} catch {
-    throw $_
-}
+    $WuaServiceManager = New-Object -ComObject 'Microsoft.Update.ServiceManager'
 
-try {
-    $ServiceRegistration = $ServiceManager.AddService2($ServiceID, $ServiceFlags, '')
-    $null = [Runtime.InteropServices.Marshal]::FinalReleaseComObject($ServiceRegistration)
+    $MuServiceRegistered = $false
+    $WuaServices = $WuaServiceManager.Services
+    for ($i = 0; $i -lt $WuaServices.Count; $i++) {
+        $WuaService = $WuaServices[$i]
+
+        if ($WuaService.ServiceID -contains $MuServiceId) {
+            $MuServiceRegistered = $true
+        }
+
+        $null = [Runtime.InteropServices.Marshal]::ReleaseComObject($WuaService)
+
+        if ($MuServiceRegistered) {
+            Write-Verbose -Message 'Microsoft Update service already registered with WUA.'
+            return
+        }
+    }
+
+    Write-Verbose -Message 'Registering Microsoft Update service with WUA ...'
+    $MuService = $WuaServiceManager.AddService2($MuServiceId, $MuServiceFlags, '')
 } catch {
     throw $_
 } finally {
-    $null = [Runtime.InteropServices.Marshal]::FinalReleaseComObject($ServiceManager)
+    if ($MuService) { $null = [Runtime.InteropServices.Marshal]::ReleaseComObject($MuService) }
+    if ($WuaServices) { $null = [Runtime.InteropServices.Marshal]::ReleaseComObject($WuaServices) }
+    if ($WuaServiceManager) { $null = [Runtime.InteropServices.Marshal]::ReleaseComObject($WuaServiceManager) }
 }
