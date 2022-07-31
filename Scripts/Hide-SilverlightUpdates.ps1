0925e246414091a8418438f05047da03e081696f
Samuel D. Leslie <sdl@nexiom.net>
2022-07-31 15:38:07 +1000

    Hide-SilverlightUpdates: Major clean-up & add documentation

--- Scripts/Hide-SilverlightUpdates.ps1
+++ Scripts/Hide-SilverlightUpdates.ps1
@@ -1,39 +1,72 @@
+<#
+    .SYNOPSIS
+    Hides Silverlight updates
+
+    .DESCRIPTION
+    On some releases of Windows which have opted-in to Microsoft Update, the Silverlight application framework may be offered.
+
+    While these updates can be hidden via the Windows Update user interface, doing so will result in the previous Silverlight update (i.e. the one which was superseded by the now hidden update) being offered.
+
+    Hiding all Silverlight updates will typically take many "scan and hide" iterations. This script will scan for and hide all Silverlight updates in a single invocation.
+
+    .EXAMPLE
+    Hide-SilverlightUpdates
+
+    Enumerates available updates and hides those with Silverlight in the title.
+
+    .NOTES
+    Administrator privileges are required to modify the visibility of updates.
+
+    This is a PowerShell implementation of a WSH solution:
+    https://superuser.com/a/1009947
+
+    .LINK
+    https://github.com/ralish/PSWinGlue
+#>
+
 #Requires -Version 3.0
 #Requires -RunAsAdministrator
 
 [CmdletBinding()]
+[OutputType()]
 Param()
 
-# PowerShell implementation of WSH solution:
-# https://superuser.com/a/1009947
+$UpdateSession = $null
+$UpdateSearcher = $null
 
 try {
-    $UpdateSession = New-Object -ComObject Microsoft.Update.Session
-} catch {
-    throw $_
-}
+    $UpdateSession = New-Object -ComObject 'Microsoft.Update.Session'
 
-try {
     $UpdateSearcher = $UpdateSession.CreateUpdateSearcher()
     $UpdateSearcher.Online = $false
+
+    do {
+        $UpdatesFound = $false
+
+        $SearchResults = $UpdateSearcher.Search('IsHidden=0 And IsInstalled=0')
+        $SearchUpdates = $SearchResults.Updates
+
+        if ($SearchUpdates.Count -gt 0) {
+            for ($i = 0; $i -lt $SearchUpdates.Count; $i++) {
+                $SearchUpdate = $SearchUpdates[$i]
+
+                if ($SearchUpdate.Title -match 'Silverlight') {
+                    $UpdatesFound = $true
+
+                    Write-Verbose -Message ('Hiding update: {0}' -f $Update.Title)
+                    $Update.IsHidden = $true
+                }
+
+                $null = [Runtime.InteropServices.Marshal]::ReleaseComObject($SearchUpdate)
+            }
+        }
+
+        $null = [Runtime.InteropServices.Marshal]::ReleaseComObject($SearchUpdates)
+        $null = [Runtime.InteropServices.Marshal]::ReleaseComObject($SearchResults)
+    } while ($UpdatesFound)
 } catch {
     throw $_
+} finally {
+    if ($UpdateSearcher) { $null = [Runtime.InteropServices.Marshal]::ReleaseComObject($UpdateSearcher) }
+    if ($UpdateSession) { $null = [Runtime.InteropServices.Marshal]::ReleaseComObject($UpdateSession) }
 }
-
-do {
-    $UpdatesFound = $false
-    $SearchResults = $UpdateSearcher.Search("IsHidden=0 And IsInstalled=0")
-
-    if ($SearchResults.Updates.Count -gt 0) {
-        foreach ($Update in ($SearchResults.Updates | Where-Object Title -Match 'Silverlight')) {
-            Write-Verbose -Message ('Hiding update: {0}' -f $Update.Title)
-            $Update.IsHidden = $true
-            $UpdatesFound = $true
-        }
-    }
-
-    $null = [Runtime.InteropServices.Marshal]::FinalReleaseComObject($SearchResults)
-} while ($UpdatesFound)
-
-$null = [Runtime.InteropServices.Marshal]::FinalReleaseComObject($UpdateSearcher)
-$null = [Runtime.InteropServices.Marshal]::FinalReleaseComObject($UpdateSession)
