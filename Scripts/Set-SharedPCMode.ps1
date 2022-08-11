35d464dcbccb5df790bccfa7fab09380c903c038
Samuel D. Leslie <sdl@nexiom.net>
2022-08-11 17:37:26 +1000

    Set-SharedPCMode: Minor clean-up

--- Scripts/Set-SharedPCMode.ps1
+++ Scripts/Set-SharedPCMode.ps1
@@ -1,6 +1,6 @@
 <#
     .SYNOPSIS
-    Configures Shared PC Mode on Windows 10
+    Configures Shared PC Mode using the SharedPC CSP via the MDM Bridge WMI Provider
 
     .DESCRIPTION
     The typical approach for configuring Shared PC Mode is to use an MDM solution which interacts with the SharedPC CSP.
@@ -20,7 +20,9 @@
     Enables Shared PC Mode with automatic account management and local storage restrictions.
 
     .NOTES
-    The MDM Bridge WMI Provider can only be interacted with from the NT AUTHORITY\SYSTEM account.
+    VPN configuration using the VPNv2 CSP is only available on Windows 10 1607 or later.
+
+    To interact with the MDM Bridge WMI Provider the script must be running as SYSTEM.
 
     Typically this script would be run non-interactively by a service running in the SYSTEM context (e.g. Group Policy Client).
 
@@ -28,19 +30,24 @@
 
     For example, the following PsExec command will launch PowerShell under the SYSTEM account: psexec -s -i powershell
 
-    Set up a shared or guest PC with Windows 10
+    Set up a shared or guest PC with Windows 10/11
     https://docs.microsoft.com/en-us/windows/configuration/set-up-shared-or-guest-pc
 
     SharedPC CSP
     https://docs.microsoft.com/en-us/windows/client-management/mdm/sharedpc-csp
 
+    MDM_SharedPC class
+    https://docs.microsoft.com/en-us/windows/win32/dmwmibridgeprov/mdm-sharedpc
+
     .LINK
     https://github.com/ralish/PSWinGlue
 #>
 
-#Requires -Version 3.0
+# Minimum supported Windows release ships with PowerShell 5.1
+#Requires -Version 5.1
 
 [CmdletBinding()]
+[OutputType([Void], [Microsoft.Management.Infrastructure.CimInstance])]
 Param(
     [Bool]$EnableSharedPCMode,
     [Bool]$SetEduPolicies,
@@ -49,47 +56,50 @@ Param(
     [Bool]$SignInOnResume,
     [Int]$SleepTimeout,
     [Bool]$EnableAccountManager,
-    [Int16]$AccountModel,
-    [Int16]$DeletionPolicy,
-    [Int16]$DiskLevelDeletion,
-    [Int16]$DiskLevelCaching,
+    [Int]$AccountModel,
+    [Int]$DeletionPolicy,
+    [Int]$DiskLevelDeletion,
+    [Int]$DiskLevelCaching,
     [Bool]$RestrictLocalStorage,
     [String]$KioskModeAUMID,
     [String]$KioskModeUserTileDisplayText,
-    [Int16]$InactiveThreshold,
-    [Int16]$MaxPageFileSizeMB,
+    [Int]$InactiveThreshold,
+    [Int]$MaxPageFileSizeMB,
 
     [Switch]$PassThru
 )
+
+$WmiNamespace = 'root\cimv2\mdm\dmmap'
+$WmiClassName = 'MDM_SharedPC'
+
+$OSRequiredType = 1         # Workstation
+$OSRequiredBuild = 14393    # Windows 10 1607
+$SidSystem = 'S-1-5-18'     # NT AUTHORITY\SYSTEM
 
 $PowerShellCore = New-Object -TypeName Version -ArgumentList 6, 0
 if ($PSVersionTable.PSVersion -ge $PowerShellCore -and $PSVersionTable.Platform -ne 'Win32NT') {
     throw '{0} is only compatible with Windows.' -f $MyInvocation.MyCommand.Name
 }
 
-$OSBuild = [Environment]::OSVersion.Version.Build
-$OSType = (Get-CimInstance -ClassName Win32_OperatingSystem).ProductType
-if ($OSBuild -lt 14393 -or $OSType -ne 1) {
-    throw 'Shared PC Mode is only available on Windows 10 1607 and newer.'
+$OSCurrentType = (Get-CimInstance -ClassName 'Win32_OperatingSystem' -Verbose:$false).ProductType
+$OSCurrentBuild = [Environment]::OSVersion.Version.Build
+if ($OSCurrentBuild -lt $OSRequiredBuild -or $OSCurrentType -ne $OSRequiredType) {
+    throw 'Shared PC Mode is only available on Windows 10 1607 or later.'
 }
 
-$SystemSid = 'S-1-5-18'
-$CurrentSid = ([Security.Principal.WindowsIdentity]::GetCurrent()).User.Value
-if ($CurrentSid -ne $SystemSid) {
+$SidCurrent = ([Security.Principal.WindowsIdentity]::GetCurrent()).User.Value
+if ($SidCurrent -ne $SidSystem) {
     throw 'Must be running as SYSTEM to interact with MDM Bridge WMI Provider.'
 }
 
-$WmiNamespace = 'root\cimv2\mdm\dmmap'
-$WmiClassName = 'MDM_SharedPC'
 $MdmSharedPC = Get-CimInstance -Namespace $WmiNamespace -ClassName $WmiClassName -ErrorAction Stop
-
 $MdmCspProperties = $MdmSharedPC.get_CimInstanceProperties().Name
 $IgnoredParameters = [Management.Automation.Cmdlet]::CommonParameters + 'PassThru'
 
 foreach ($Parameter in $PSBoundParameters.GetEnumerator()) {
     if ($Parameter.Key -notin $IgnoredParameters) {
         if ($Parameter.Key -notin $MdmCspProperties) {
-            throw ('Parameter not supported on this Windows 10 version: {0}' -f $Parameter.Key)
+            throw 'Parameter not supported on this Windows 10 version: {0}' -f $Parameter.Key
         }
 
         $MdmSharedPC.($Parameter.Key) = $Parameter.Value
