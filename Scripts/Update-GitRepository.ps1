37ecd54fd8b0d266f0799196e23ad3ced04a3961
Samuel D. Leslie <sdl@nexiom.net>
2022-08-14 21:18:23 +1000

    Update-GitRepository: Major clean-up & add documentation

--- Scripts/Update-GitRepository.ps1
+++ Scripts/Update-GitRepository.ps1
@@ -1,10 +1,49 @@
+<#
+    .SYNOPSIS
+    Updates a Git repository
+
+    .DESCRIPTION
+    A helper function to automate updating a Git repository.
+
+    The following steps are performed with a failure aborting subsequent steps:
+    - Check the current directory is a valid Git repository
+    - Add all changes in the working tree to the staging area
+    - If there are staged changes commit them to the repository
+    - Pull changes from upstream and perform a fast-forward merge
+    - Push the new commit(s) to the upstream repository
+
+    .EXAMPLE
+    Update-GitRepository
+
+    Updates the Git repository in the current directory. See the description section for the steps performed.
+
+    .NOTES
+    This script is primarily intended for usage in unattended scenarios, such as invoking from a Scheduled Task.
+
+    The following potential issues are handled, which are more common with or specific to Scheduled Task execution:
+    - Checks Git is installed, and if so, retrieves its installation directory
+      This enables the script to handle the case where the Git binaries aren't in the PATH of the executing user.
+    - Ensures the environment is initialised for Git to function correctly
+      In particular, the HOME environment variable may be missing when run as a Scheduled Task, causing SSH issues.
+
+    .LINK
+    https://github.com/ralish/PSWinGlue
+#>
+
 #Requires -Version 3.0
 
 [CmdletBinding()]
+[OutputType([String[]])]
 Param()
+
+$PowerShellCore = New-Object -TypeName Version -ArgumentList 6, 0
+if ($PSVersionTable.PSVersion -ge $PowerShellCore -and $PSVersionTable.Platform -ne 'Win32NT') {
+    throw '{0} is only compatible with Windows.' -f $MyInvocation.MyCommand.Name
+}
 
 Function Initialize-GitEnvironment {
     [CmdletBinding()]
+    [OutputType([Void])]
     Param(
         [Parameter(Mandatory)]
         [String]$Path
@@ -15,16 +54,19 @@ Function Initialize-GitEnvironment {
 
     # Create the HOME environment variable needed by SSH (via Git)
     #
-    # If running as a Scheduled Task the user profile of the account may not yet be loaded on
-    # Windows 8 or Server 2012 and newer. This results in the USERPROFILE environment variable
-    # pointing to the Default user profile. We can work around this by using GetFolderPath().
+    # When running as a Scheduled Task the user profile of the account may not
+    # yet be loaded on Windows 8 or Server 2012 and newer. This can cause the
+    # USERPROFILE environment variable to point to the Default user profile. A
+    # workaround is to retrieve and set the correct path using GetFolderPath().
     #
-    # See: https://support.microsoft.com/en-us/kb/2968540
+    # Scheduled tasks reference incorrect user profile paths
+    # https://support.microsoft.com/en-us/kb/2968540
     $Env:HOME = [Environment]::GetFolderPath([Environment+SpecialFolder]::UserProfile)
 }
 
 Function Test-GitInstalled {
     [CmdletBinding()]
+    [OutputType([String])]
     Param()
 
     # Registry keys potentially containing Git installation details
@@ -41,37 +83,42 @@ Function Test-GitInstalled {
     $GitInstallProperty = 'InstallLocation'
 
     foreach ($RegKey in $GitRegistryKeys) {
-        $GitInstallPath = (Get-ItemProperty -Path $RegKey -ErrorAction Ignore).$GitInstallProperty
-
-        if ($GitInstallPath) {
-            break
+        try {
+            $RegKeyProps = Get-ItemProperty -Path $RegKey -ErrorAction Stop
+            $GitInstallPath = $RegKeyProps.$GitInstallProperty
+        } catch {
+            continue
         }
+
+        if ([String]::IsNullOrWhiteSpace($GitInstallPath)) {
+            continue
+        }
+
+        if (!(Test-Path -Path $GitInstallPath -PathType Container)) {
+            continue
+        }
+
+        return $GitInstallPath
     }
 
-    if (!$GitInstallPath) {
-        throw 'Unable to locate a Git installation.'
-    }
-
-    if (!(Test-Path -Path $GitInstallPath -PathType Container)) {
-        throw 'The Git installation directory does not exist.'
-    }
-
-    return $GitInstallPath
+    throw 'Unable to locate a Git installation.'
 }
 
 Function Test-GitRepository {
     [CmdletBinding()]
+    [OutputType([Void])]
     Param()
 
     $null = & git rev-parse --git-dir 2>&1
     if ($LASTEXITCODE -ne 0) {
-        throw ('The current directory does not belong to a Git repository: {0}' -f $PWD.Path)
+        throw 'The current directory does not belong to a Git repository: {0}' -f $PWD.Path
     }
 }
 
 Function Update-GitRepository {
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
     [CmdletBinding()]
+    [OutputType([String[]])]
     Param()
 
     & git add --all
