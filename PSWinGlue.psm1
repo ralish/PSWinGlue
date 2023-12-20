0a3a819658d0ababc486b175375d0295031a6241
Samuel D. Leslie <sdl@nexiom.net>
2023-12-20 12:32:01 +1100

    Show a warning on skipping import of existing functions

--- PSWinGlue.psm1
+++ PSWinGlue.psm1
@@ -4,5 +4,13 @@ Set-StrictMode -Version 2.0
 # Import all scripts as functions
 $Scripts = Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Scripts') -File
 foreach ($Script in $Scripts) {
-    New-Item -Path ('Function:\{0}' -f $Script.BaseName) -Value (Get-Content -Path $Script.FullName -Raw)
+    $FunctionName = $Script.BaseName
+    $FunctionPath = 'Function:\{0}' -f $FunctionName
+
+    if (Test-Path -Path $FunctionPath) {
+        Write-Warning -Message ('Skipping import of existing function: {0}' -f $FunctionName)
+        continue
+    }
+
+    New-Item -Path $FunctionPath -Value (Get-Content -Path $Script.FullName -Raw)
 }
